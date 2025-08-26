1cec0ac67ef8e852d1f6d96218a55099877966c6
Dmitriy Koluzayev <tornadomg.team@gmail.com>
2025-08-26 21:19:32 +0500

    ОДобавлены докстринги

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,7 +2,7 @@ from src.processing import filter_by_state, sort_by_date
 
 
 def test_filter_by_state(fixture_filter_by_state: list[dict]) -> None:
-    """Тестовая параметризация для файла processing.py и функции filter_by_state"""
+    """Тестовая фикстура для файла processing.py и функции filter_by_state"""
     assert filter_by_state(fixture_filter_by_state) == [
         {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
@@ -14,7 +14,7 @@ def test_filter_by_state(fixture_filter_by_state: list[dict]) -> None:
 
 
 def test_sort_by_date(fixture_filter_by_state: list[dict]) -> None:
-    """Тестовая параметризация для файла processing.py и функции sort_by_date"""
+    """Тестовая фикстура для файла processing.py и функции sort_by_date"""
     assert sort_by_date(fixture_filter_by_state) == [
         {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
         {"date": "2018-09-12T21:27:25.241689", "id": 594226727, "state": "CANCELED"},

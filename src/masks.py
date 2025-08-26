db8dbe96b5cfd69d4536265137550b64e80c9078
Dmitriy Koluzayev <tornadomg.team@gmail.com>
2025-08-26 21:05:52 +0500

    исправлена функция

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,14 @@
-def get_mask_card_number(card_number: str) -> str:
+def get_mask_card_number(card_number: str | int) -> str:
     """принимает на вход номер карты и возвращает ее маску"""
-    try:
+    if isinstance(card_number, str):
         masked_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
         return masked_card
-    except Exception:
-        raise TypeError("Неверный формат аргумента")
+    raise TypeError("Неверный формат аргумента")
 
 
-def get_mask_account(account_number: str) -> str:
+def get_mask_account(account_number: str | int) -> str:
     """принимает на вход номер счета и возвращает его маску"""
-    try:
+    if isinstance(account_number, str):
         masked_account = f"**{account_number[-4:]}"
         return masked_account
-    except Exception:
-        raise TypeError("Неверный формат аргумента")
+    raise TypeError("Неверный формат аргумента")
